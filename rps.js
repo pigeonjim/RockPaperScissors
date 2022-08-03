73e908cd13fef4b1d565b2802a9e474cc127e7d7
James <pigeonjim@gmail.com>
2022-08-03 08:29:20 +0100

    final coding and testing

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    const choiceArry = ['Rock', 'Paper', 'Scissors'];
+    const choiceArry = ['rock', 'paper', 'scissors'];
     let randChoice = Math.floor(Math.random() * choiceArry.length);
     return choiceArry[randChoice];    
 }
@@ -7,28 +7,61 @@ function getComputerChoice(){
 function playerSelection(){
     const choices = ['paper', 'scissors', 'rock']
     let pChoice =  window.prompt('Please make a choice');
-    if(!(choices.includes(pChoice.toLowerCase()))){
+    while(!(choices.includes(pChoice.toLowerCase()))){
         console.log("invalid choice");
         pChoice =  window.prompt('Please make a choice');
-    } else{
-    return pChoice;}
+    } 
+    return pChoice.toLowerCase();    
 }
 
 function playRound(computerMove, playerMove){
-    if(computerMove.toLowercase() === playerMove.toLowercase()){
+    if(computerMove === playerMove){
         return "It's a Draw!";
-    }  else if(computerMove.toLowercase() === "stone" && playerMove.toLowercase() === "paper"){
-        return "You win. Paper wraps stone";
-    } else if(computerMove.toLowercase() === "paper" && playerMove.toLowercase() === "scissors"){
-        return "You Win. Scissors cut paper";
-    } else if(computerMove.toLowercase() === "scissors" && playerMove.toLowercase() === "stone"){
-        return "You win. Stone blunts scissors";
-    } else if(computerMove.toLowercase() === "scissors" && playerMove.toLowercase() === "paper"){
+    }  else if(computerMove === "rock" && playerMove === "paper"){
+        return "You win. Paper wraps rock";
+    } else if(computerMove === "paper" && playerMove === "scissors"){
+        return "You win. Scissors cut paper";
+    } else if(computerMove === "scissors" && playerMove === "rock"){
+        return "You win. rock blunts scissors";
+    } else if(computerMove === "scissors" && playerMove === "paper"){
         return "You lose. Scissors cut paper";
-    }else if(computerMove.toLowercase() === "paper" && playerMove.toLowercase() === "stone"){
-        return "You lose. Paper wraps stone";
-    }else if(computerMove.toLowercase() === "stone" && playerMove.toLowercase() === "scissors"){
-        return "You lose. Stone blunts scissors";
-    } 
+    }else if(computerMove === "paper" && playerMove === "rock"){
+        return "You lose. Paper wraps rock";
+    }else if(computerMove === "rock" && playerMove === "scissors"){
+        return "You lose. rock blunts scissors";
+    } else{
+        console.log("logic error here");
+        return "you lose";
+    }
+}
 
+function game(){
+    let cScore = 0;
+    let pScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        let cMove = getComputerChoice();
+        let pMove = playerSelection();
+        let result = playRound(cMove,pMove);
+        winOrLose(result) === "player" ? pScore++ : cScore++;
+    }
+
+    if(pScore > cScore){
+        console.log(`Score - Player : ${pScore} - Compuer : ${cScore} . You Win!`);
+    } else if (pScore < cScore){
+        console.log(`Score - Player : ${pScore} - Compuer : ${cScore} . You lose :-(`);
+    } else{
+        console.log(`Score - Player : ${pScore} - Compuer : ${cScore} . It's a draw!`);
+    }
+
+}
+
+function winOrLose(result){
+    if(result.includes("win")){
+        console.log("player");
+        return "player";
+    } else {
+        console.log("computer");
+        return "computer";
+    }
 }
